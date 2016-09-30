0e35a3241f2f9ad303cc46e9fba80e9135b2c5af
Konrad Straszewski <koonradstraszewski@gmail.com>
2016-09-30 17:39:35 +0200

    optymalizacja

--- src/factory/listAccess/listAccess.js
+++ src/factory/listAccess/listAccess.js
@@ -1,43 +1,36 @@
 app.factory('listAccess', function ($log) {
 
-    var list1 = [
-        "Marek1",
-        "Marek2",
-        "Marek3",
-        "Marek4",
-        "Marek5"
+    var list = [
+        [
+            "Marek1",
+            "Marek2",
+            "Marek3",
+            "Marek4",
+            "Marek5"
+        ],
+        [
+            "Wojciech1",
+            "Wojciech2",
+            "Wojciech3",
+            "Wojciech4",
+            "Wojciech5"
+        ]
     ];
 
-    var list2 = [
-        "Wojciech1",
-        "Wojciech2",
-        "Wojciech3",
-        "Wojciech4",
-        "Wojciech5"
-    ];
-
-    var moveElementById = function (element) {
-        $log.debug(list1.indexOf(element));
-
-        if (list1.indexOf(element) != -1) {
-            list2.push(element);
-            list1.splice(list1.indexOf(element), 1);
-        } else {
-            list1.push(element);
-            list2.splice(list2.indexOf(element), 1);
-        }
+    var moveElementById = function (element, listId) {
+        list[listId].splice(list[listId].indexOf(element),1);
+        list[+!+listId].push(element);
     };
 
     var addNewElement = function (name, listNumber) {
         if (listNumber) {
-            list1.push(name);
+            list[0].push(name);
         } else {
-            list2.push(name);
+            list[1].push(name);
         }
     };
     return {
-        list1: list1,
-        list2: list2,
+        list: list,
         moveElementById: moveElementById,
         addNewElement: addNewElement
     }
